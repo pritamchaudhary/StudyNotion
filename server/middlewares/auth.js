9021d2367601eb0cea301354b4cb8d454a4656d3
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2024-03-21 23:35:40 +0530

    completed Auth.js file

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,52 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+const User = require('../models/User')
+
 // auth
+exports.auth = async(req, res, next) => {
+    try {
+        // extract token
+        const token = req.cookies.token || req.body.token || req.header("Authorisation").replace("Bearer ", "");
+
+        // if token missing, then return response
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:'Token is missing',
+            })
+        }
+        // verify the token
+        try {
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+            req.user = decode;
+        } catch (error) {
+            // verification issue
+            return res.status(401).json({
+                success:false,
+                message:'Token is invalid',
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success:false,
+            message:'Something went wrong while validating the token',
+        })
+    }
+}
 
 // isStudent
+exports.isStudent = async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'User role cannot be verified, please try again',
+        })
+    }
+}
 
 // isInstructor
 
