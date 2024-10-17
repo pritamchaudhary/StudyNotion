068c173008d0211277004f0b71082ec7af149fb4
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2024-10-17 19:41:54 +0530

    some changes in User.js

--- server/models/User.js
+++ server/models/User.js
@@ -11,6 +11,11 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
   password: {
     type: String,
     required: true,
