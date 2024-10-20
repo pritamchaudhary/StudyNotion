3237ccdf2975775fab9e424d64048cb51ff65ad0
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2024-10-20 19:30:33 +0530

    replace tags to categories

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -1,8 +1,8 @@
-const Tag = require("../models/Tags");
+const Category = require("../models/Category");
 
 // createTag handler function
 
-exports.createTag = async (req, res) => {
+exports.createCategory = async (req, res) => {
     try {
         // fetch data from req.body
         const {name, description} = req.body;
@@ -16,16 +16,16 @@ exports.createTag = async (req, res) => {
         }
 
         // tag entry in db
-        const tagDetails = await Tag.create({
+        const categoryDetails = await Category.create({
             name: name,
             description: description,
         })
-        console.log(tagDetails);
+        console.log(categoryDetails);
 
         // return response
         return res.status(200).json({
             success: true,
-            message: 'Tag Created Successfully',
+            message: 'Category Created Successfully',
         })
 
     } catch (error) {
@@ -38,13 +38,13 @@ exports.createTag = async (req, res) => {
 
 // getAllTags handler function
 
-exports.showAllTags = async(req, res) => {
+exports.showAllCategories = async(req, res) => {
     try {
-       const allTags = await Tag.find({}, {name:true, description:true});
+       const allCategories = await Category.find({}, {name:true, description:true});
        
        res.status(200).json({
         success: true,
-        message: 'All tags returned successfully',
+        message: 'All Categories returned successfully',
         allTags,
        })
     } catch (error) {
