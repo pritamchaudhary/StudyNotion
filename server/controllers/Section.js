b1877d1ad53a9c341d7291d1af5bd9956ef62bdc
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2024-10-21 14:51:07 +0530

    complete createSection, updateSection and deleteSection handler function in Section.js

---
+++ server/controllers/Section.js
@@ -0,0 +1,108 @@
+const Section = require("../models/Section");
+const Course = require("../models/Course");
+
+// createSection
+
+exports.createSection = async (req, res) => {
+  try {
+    // data fetch
+    const { sectionName, courseId } = req.body;
+
+    // data validation
+    if (!sectionName || !courseId) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing Properties",
+      });
+    }
+
+    // create section
+    const newSection = await Section.create({ sectionName });
+
+    // update course with section ObjectID
+    const updatedCourseDetails = await Course.findByIdAndUpdate(
+      courseId,
+      {
+        $push: {
+          courseContent: newSection._id,
+        },
+      },
+      { new: true }
+    );
+
+    // return response
+    return res.status(200).json({
+      success: true,
+      message: "Section created successfully",
+      updatedCourseDetails,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Unable to create Section, please try again",
+      error: error.message,
+    });
+  }
+};
+
+// updatedSection 
+
+exports.updateSection = async (req, res) => {
+  try {
+    // data input
+    const { sectionName, sectionId } = req.body;
+
+    // data validation
+    if (!sectionName || !sectionId) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing Properties",
+      });
+    }
+
+    // update data
+    const section = await Section.findByIdAndUpdate(
+      sectionId,
+      { sectionName },
+      { new: true }
+    );
+
+    // return res
+    return res.status(200).json({
+      success: true,
+      message: "Section Updated Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Unable to create Section, please try again",
+      error: error.message,
+    });
+  }
+};
+
+// deleteSection
+
+exports.deleteSection = async (req, res) => {
+  try {
+    // get ID - assuming that we are sending ID in params
+    const { sectionId } = req.params;
+
+    // use findByIdAndDelete
+    await Section.findByIdAndDelete(sectionId);
+
+    // TODO[testing]: do we need to delete the entry from the course schema ??
+
+    // return res
+    return res.status(200).json({
+      success: true,
+      message: "Section Deleted Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Unable to delete Section, please try again",
+      error: error.message,
+    });
+  }
+};
